c321ce104ea704be6e7ae2fa1f42aaa83248bda0
Ivo Calado <ivo.calado@ifal.edu.br>
2022-01-31 19:40:50 -0300

    Update tfsec scripts
    Signed-off-by: Ivo Calado <ivo.calado@ifal.edu.br>

--- tfsec_analysis.sh
+++ tfsec_analysis.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-url=`wget -q https://api.github.com/repos/aquasecurity/tfsec/releases/latest -O - | grep -o -E 'https://.+?tfsec-linux-amd64' | head -n1`;
+#url=`wget -q https://api.github.com/repos/aquasecurity/tfsec/releases/latest -O - | grep -o -E 'https://.+?tfsec-linux-amd64' | head -n1`;
+url=https://github.com/aquasecurity/tfsec/releases/download/v0.63.1/tfsec-linux-amd64
 echo ">> URL: $url";
 wget -O - -q $url > tfsec;
 echo ">>Installling tfsec";

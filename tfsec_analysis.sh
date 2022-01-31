6318e71b8b0aa363ba61274b8b2be519e1c140e1
Ivo Calado <ivo.calado@ifal.edu.br>
2022-01-31 19:17:12 -0300

    Update tfsec
    Signed-off-by: Ivo Calado <ivo.calado@ifal.edu.br>

--- tfsec_analysis.sh
+++ tfsec_analysis.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Hello tfsec"
+url=`wget -q https://api.github.com/repos/aquasecurity/tfsec/releases/latest -O - | grep -o -E 'https://.+?tfsec-linux-amd64' | head -n1`;
+echo ">> URL: $url";
+wget -O - -q $url > tfsec;
+echo ">>Installling tfsec";
+install tfsec /usr/local/bin/;
+echo ">>Running tfsec";
+./bin/tfsec_scan.sh
